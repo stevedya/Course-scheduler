f882149d1e80b76c309c1f076d3bcec6cebf966d
Steven Steinwand <steven_ts@hotmail.com>
2017-11-20 08:22:18 -0700

    Fixed with king nathan code

--- js/models/course.js
+++ js/models/course.js
@@ -1,57 +1,105 @@
-(function(exports) {
+(function (exports) {
 
-	var app = exports.app || (exports.app = {}),
+    var app = exports.app || (exports.app = {}),
 
-		Course = Backbone.Model.extend({
-			defaults: {
-				code: '',
-				name: '',
-				instructor: '',
-				classes: [{
-					day: '',
-                    start: '',
-                    end: ''
-				}]
-			},
+        Course = Backbone.Model.extend({
+            defaults: {
+                code: '',
+                name: '',
+                instructor: '',
+                classes: []
+            },
 
-			// TODO: include a function to add class times (e.g. addClassTime(time))
-			addClassTime: function(time) {
-                this.get('classes').push(time);
-                this.validate();
-			},
-
-			validate: function(attrs) {
-				// TODO: modify this validation to suit your needs
-			    if (attrs.hasOwnProperty('name') && _.isEmpty(attrs.name)) {
-			        return 'Course name cannot be empty.';
-			    }
-                if (attrs.hasOwnProperty('code') && _.isEmpty(attrs.code)) {
-                    return 'code name cannot be empty.';
+            /**
+             * Adds a class time object to this course.
+             * @param {Object} classTime - a single class time representation
+             * @param {string} classTime.day - the day of the week for the class
+             * @param {string} classTime.start - the start time for the class
+             * @param {string} classTime.end - the end time for the class
+             * @returns {Boolean|undefined} - the result from calling the validate() function or undefined if the classTime did not have the required attributes
+             */
+            addClass: function (classTime) {
+                // light validation at this level
+                if (_.isObject(classTime) && classTime.hasOwnProperty('day') && classTime.hasOwnProperty('start') && classTime.hasOwnProperty('end')) {
+                    this.attributes.classes.push(classTime);
+                    return this.isValid(); // trigger full validation
                 }
-                if (attrs.hasOwnProperty('instructor') && _.isEmpty(attrs.instructor)) {
-                    return 'instructor name cannot be empty.';
+            },
+
+            /**
+             * Removes a class time from this course.
+             * @param {Object|number} value - the actual class time object to remove or the index of the class to remove
+             * @returns {Object|undefined} - the removed class time object if successful or undefined if no object was removed
+             */
+            removeClass: function (value) {
+                var idx;
+
+                if (_.isNumber(value)) {
+                    // remove by index
+                    if (value >= 0 && value < this.attributes.classes.length) {
+                        return this.attributes.classes.splice(value, 1);
+                    }
+                } else if (_.isObject(value)) {
+                    // remove by ref
+                    if ((idx = this.attributes.classes.indexOf(value)) > -1) {
+                        return this.attributes.classes.splice(idx, 1);
+                    }
                 }
-			    // TODO: complete validation for the remaining attributes
-				if (attrs.hasOwnProperty('classes')) {
-			    	attrs.classes.forEach(function (classTime) {
-						if(_.isEmpty(classTime.day)) {
-							return 'class day cannot be empty.';
-						}
-                        if(_.isEmpty(classTime.start)) {
-                            return 'class start cannot be empty.';
-                        }
-                        if(_.isEmpty(classTime.end)) {
-                            return 'class end cannot be empty.';
-                        }
+            },
+
+            validate: function (attrs) {
+                var errors = [];
+
+                if (attrs.hasOwnProperty('name') && _.isEmpty(attrs.name)) {
+                    errors.push({
+                        attr: 'name',
+                        message: 'Course.name must not be an empty string.'
                     });
-				}
-			    // classes can be a bit 'tricky'
-			}
-		});
+                }
+
+                if (attrs.hasOwnProperty('code') && _.isEmpty(attrs.code)) {
+                    errors.push({
+                        attr: 'code',
+                        message: 'Course.code must not be an empty string.'
+                    });
+                }
+
+                if (attrs.hasOwnProperty('instructor') && _.isEmpty(attrs.instructor)) {
+                    errors.push({
+                        attr: 'instructor',
+                        message: 'Course.instructor must not be an empty string.'
+                    });
+                }
+
+                // for classes, need to ensure that an array of {day, start, end} objects are present
+                if (attrs.hasOwnProperty('classes') && !_.isArray(attrs.classes)) {
+                    errors.push({
+                        attr: 'classes',
+                        message: 'Course.classes must be an array.'
+                    });
+                } else if (attrs.classes.length > 0) {
+                    // array present, check for objects
+                    attrs.classes.forEach(function (session) {
+                        if (!session.hasOwnProperty('day') || !session.hasOwnProperty('start') || !session.hasOwnProperty('end')) {
+                            errors.push({
+                                attr: 'classes',
+                                message: 'Course.classes must be an array of {day, start, end} objects.'
+                            });
+                        }
+
+                    });
+                }
+
+                // only return a value if any errors were found (as per the Backbone documentation)
+                if (errors.length > 0) {
+                    return errors;
+                }
+            }
+        });
 
 
-	// export the Course model
-	app.models || (app.models = {});
-	app.models.Course = Course;
+    // export the Course model
+    app.models || (app.models = {});
+    app.models.Course = Course;
 
 }(this));
--- test/models/test.course.js
+++ test/models/test.course.js
@@ -16,74 +16,195 @@ describe('Course', function () {
         it('has an instructor set to \'\'', function () {
             expect(course.get('instructor')).toEqual('');
         });
-        it('has an classes set to \'\'', function () {
-            expect(course.get('classes')).toEqual(
-                [{
-                    day: '',
-                    start: '',
-                    end: ''
-                }]
-            );
+        it('has scheduled classes', function() {
+            expect(course.get('classes')).toEqual(jasmine.any(Array));
         });
     });
 
 
-    describe('validates set attributes', function () {
+    describe('performs validation', function() {
+        it('returns an array of error objects if validation fails for any attribute', function() {
+            // trigger validate, default attributes for name, code, and instructor are invalid
+            course.isValid();
 
-        it('ensures a non-empty name is provided', function () {
-            var errorCallback = jasmine.createSpy('-invalid event callback-');
-
-            course.on('invalid', errorCallback);
-            course.set({name: ''}, {validate: true}); //can't be empty
-
-            var errorArgs = errorCallback.calls.mostRecent().args;
-
-            expect(errorArgs).toBeDefined();
-            expect(errorArgs[0]).toBe(course);
-            expect(errorArgs[1]).toBe('Course name cannot be empty.');
+            // check for appropriate validationError
+            expect(course.validationError).toEqual(jasmine.any(Array));
+            expect(course.validationError.length).toEqual(3);
         });
 
-        it('Ensures a non-empty course code is provided', function () {
-            var errorCallback = jasmine.createSpy('-invalid event callback-');
+        describe('validates set attributes', function() {
 
-            course.on('invalid', errorCallback);
-            course.set({name: 'Food Studies', code: ''}, {validate: true});  //Cant be empty
+            // ************************************************************************
+            // Earlier form of validation test. Tests have been updated/added to be
+            // simpler to check and provide a means to return multiple error messages.
+            // ************************************************************************
 
-            var errorArgs = errorCallback.calls.mostRecent().args;
-
-            expect(errorArgs).toBeDefined();
-            expect(errorArgs[0]).toBe(course);
-            expect(errorArgs[1]).toBe('code name cannot be empty.');
-        });
-
-        it('Ensures a non-empty instructor code is provided', function () {
-            var errorCallback = jasmine.createSpy('-invalid event callback-');
-
-            course.on('invalid', errorCallback);
-            course.set({name: 'Food Studies', code: 'COMP1002', instructor: ''}, {validate: true});  //Cant be empty
-
-            var errorArgs = errorCallback.calls.mostRecent().args;
-
-            expect(errorArgs).toBeDefined();
-            expect(errorArgs[0]).toBe(course);
-            expect(errorArgs[1]).toBe('instructor name cannot be empty.');
-        });
-
-
-        // TODO: create specific tests for classes -> day, start, and end times
-        describe('validates set classes attributes', function () {
-            it('ensures class day was entered', function () {
-                var errorCallback = jasmine.createSpy('-invalid event callback-');
+            /*
+            it('ensures a non-empty name is provided', function() {
+                var errorArgs,
+                    errorCallback = jasmine.createSpy('-invalid event callback-');
 
                 course.on('invalid', errorCallback);
-                course.set({name: 'Food Studies', code: 'COMP1002', instructor: 'Gary'}, {validate: true});
-                course.addClassTime({day: '', start: '8:00am', end: 'NEVER'});
 
-                var errorArgs = errorCallback.calls.mostRecent().args;
+                // What would you need to set on the course attributes to
+                // cause validation to fail?
+
+                course.set({ name: '' }, { validate: true });
+
+                errorArgs = errorCallback.calls.mostRecent().args;
 
                 expect(errorArgs).toBeDefined();
                 expect(errorArgs[0]).toBe(course);
-                expect(errorArgs[1]).toBe('class day cannot be empty.');
+                expect(errorArgs[1]).toBe('Course.name must not be an empty string.');
+
+                // What would be acceptable?
+                course.set({ name: 'test name' }, { validate: true });
+
+                // Should only have been called once for the previous failure
+                expect(errorCallback.calls.count()).toEqual(1);
+            });
+
+            Updated testing begins below...
+
+            */
+
+            beforeEach(function() {
+                // set course with good values in order to test desired validation (all attributes are passed to validate function)
+                course.set({
+                    name: 'Computing 1',
+                    code: 'COMP1000',
+                    instructor: 'Jane Doe',
+                    classes: [{ day: 'Wednesday', start: '1:00PM', end: '3:00PM' }, { day: 'Friday', start: '10:00AM', end: '12:00PM' }]
+                });
+            });
+
+            it('ensures a non-empty name is provided', function() {
+                // don't need to check spy callback, can simply check validationError value
+
+                // invalid value for name
+                course.set({ name: '' }, { validate: true });
+
+                // validate will return an array of error objects if validation fails for any attribute
+                expect(course.validationError[0].attr).toEqual('name');
+                expect(course.validationError[0].message).toEqual('Course.name must not be an empty string.');
+
+                // What would be acceptable?
+                course.set({ name: 'Computing 2' }, { validate: true });
+
+                expect(course.validationError).toBeNull();
+            });
+
+            it('ensures a non-empty code is provided', function() {
+                // invalid value for code
+                course.set({ code: '' }, { validate: true });
+
+                // validate will return an array of error objects if validation fails for any attribute
+                expect(course.validationError[0].attr).toEqual('code');
+                expect(course.validationError[0].message).toEqual('Course.code must not be an empty string.');
+
+                // What would be acceptable?
+                course.set({ code: 'COMP2000' }, { validate: true });
+
+                expect(course.validationError).toBeNull();
+            });
+
+            it('ensures a non-empty instructor is provided', function() {
+                // invalid value for instructor
+                course.set({ instructor: '' }, { validate: true });
+
+                // validate will return an array of error objects if validation fails for any attribute
+                expect(course.validationError[0].attr).toEqual('instructor');
+                expect(course.validationError[0].message).toEqual('Course.instructor must not be an empty string.');
+
+                // What would be acceptable?
+                course.set({ instructor: 'COMP2000' }, { validate: true });
+
+                expect(course.validationError).toBeNull();
+            });
+
+            it('ensures that classes must be an array of {day, start, end} objects', function() {
+                // invalid type for classes
+                course.set({ classes: '' }, { validate: true });
+
+                // validate will return an array of error objects if validation fails for any attribute
+                expect(course.validationError[0].attr).toEqual('classes');
+                expect(course.validationError[0].message).toEqual('Course.classes must be an array.');
+
+                // invalid array element(s) for classes
+                course.set({ classes: ['foo', 'bar'] }, { validate: true });
+
+                // validate will return an array of error objects if validation fails for any attribute
+                expect(course.validationError[0].attr).toEqual('classes');
+                expect(course.validationError[0].message).toEqual('Course.classes must be an array of {day, start, end} objects.');
+
+                // What would be acceptable?
+                course.set({ classes: [{ day: 'Wednesday', start: '1:00PM', end: '3:00PM' }, { day: 'Friday', start: '10:00AM', end: '12:00PM' }] }, { validate: true });
+
+                expect(course.validationError).toBeNull();
+
+            });
+        });
+
+
+        // ************************************************************************
+        // the following are not required for grading (i.e. you were not expected
+        // to complete them), but may be required for functionality
+        // ************************************************************************
+        describe('supports adding class times', function() {
+            it('only allows adding {day, start, end} objects', function() {
+                // test with invalid value
+                course.addClass('');
+
+                expect(course.get('classes').length).toEqual(0);
+
+                // test with invalid object
+                course.addClass({ start: '1:00PM', end: '3:00PM' });
+                expect(course.get('classes').length).toEqual(0);
+
+                // test with acceptable object
+                course.addClass({ day: 'Wednesday', start: '1:00PM', end: '3:00PM' });
+                expect(course.get('classes').length).toEqual(1);
+                course.addClass({ day: 'Friday', start: '12:00PM', end: '2:00PM' });
+                expect(course.get('classes').length).toEqual(2);
+
+                // UPGRADE: ensure that class times cannot be duplicated or overlap
+            });
+        });
+
+        describe('supports removing class times', function() {
+            var removedClass;
+
+            beforeEach(function() {
+                // direct setting of good classes array
+                course.attributes.classes = [
+                    { day: 'Monday', start: '1:00PM', end: '3:00PM' },
+                    { day: 'Tuesday', start: '9:00AM', end: '11:00AM' },
+                    { day: 'Friday', start: '10:00AM', end: '12:00PM' }
+                ];
+            });
+
+            it('removes class time objects', function() {
+                var classToRemove;
+
+                // random invalid reference
+                course.removeClass('');
+                expect(course.get('classes').length).toEqual(3);
+
+                // good reference, remove the second class time
+                removedClass = course.removeClass(course.get('classes')[1]);
+                expect(course.get('classes').length).toEqual(2);
+                expect(course.get('classes').indexOf(removedClass)).toBe(-1);
+            });
+
+            it('removes class time objects by index', function() {
+                // random invalid index
+                course.removeClass(3);
+                expect(course.get('classes').length).toEqual(3);
+
+                // good index
+                removedClass = course.removeClass(1);
+                expect(course.get('classes').length).toEqual(2);
+                expect(course.get('classes').indexOf(removedClass)).toBe(-1);
             });
         });
 
